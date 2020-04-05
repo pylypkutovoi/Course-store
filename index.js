f79c6cbd6c43be978da1cfe090a7a9a4c15a5483
pylyp <pylyp.kutov@gmail.com>
2020-04-05 20:26:10 +0200

    register routes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const homeRoutes = require("./routes/home");
+const coursesRoutes = require("./routes/courses");
+const addRoutes = require("./routes/add");
 
 const app = express();
 
@@ -12,32 +15,10 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(express.static('public'))
-
-
-app.get('/', (req, res) => {
-
-  res.render('index.hbs', {
-    title: 'Home page',
-    isHome: true
-  });
-})
-
-app.get('/courses', (req, res) => {
-
-  res.render('courses.hbs', {
-    title: 'Courses',
-    isCourses: true
-  });
-})
-
-app.get('/add', (req, res) => {
-
-  res.render('add.hbs', {
-    title: "Add course",
-    isAdd: true
-  });
-})
+app.use(express.static('public'));
+app.use('/', homeRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/add', addRoutes)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
