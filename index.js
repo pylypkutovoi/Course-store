d83ce44a265aaf07009515901cf87f24366e3ada
pylyp <pylyp.kutov@gmail.com>
2020-04-05 19:38:39 +0200

    add basic layout

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+app.get('/', (req, res) => {
+
+  res.render('index.hbs');
+})
+
+app.get('/about', (req, res) => {
+
+  res.render('about.hbs');
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
+
 })
