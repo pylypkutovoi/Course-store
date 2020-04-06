40d236772de66351af3a23a36d9bf5e88a9e04b2
pylyp <pylyp.kutov@gmail.com>
2020-04-06 18:31:16 +0200

    create course page and empty layout

--- models/course.js
+++ models/course.js
@@ -55,6 +55,11 @@ class Course {
     })
   }
 
+  static async getById(id) {
+    const courses = await Course.getAll();
+    return courses.find(c => c.id === id);
+  }
+
 }
 
 module.exports = Course;
