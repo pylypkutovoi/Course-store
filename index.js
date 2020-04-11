b2b520f135203d5b8060646b8044700c70bdf7dd
pylyp <pylyp.kutov@gmail.com>
2020-04-11 22:00:12 +0200

    add sessions, store sessions in MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 const path = require('path');
 const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home');
@@ -10,8 +12,10 @@ const addRoutes = require('./routes/add');
 const cartRoutes = require('./routes/cart');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
-const User = require('./models/user');
+// const User = require('./models/user');
+const varMiddleware = require('./middleware/variables');
 
+const MONGODB_URI = 'mongodb://localhost:27017/shop';
 const app = express();
 
 const hbs = exphbs.create({
@@ -20,22 +24,35 @@ const hbs = exphbs.create({
   handlebars: allowInsecurePrototypeAccess(Handlebars)
 });
 
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: MONGODB_URI
+})
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5e8f614f04851f0694a9cb7a');
-    req.user = user;
-    next();
-  } catch (error) {
-    console.log(error);
-  }
-})
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById('5e8f614f04851f0694a9cb7a');
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+  secret: 'some secret',
+  resave: false,
+  saveUninitialized: false,
+  store //store: store
+}));
+app.use(varMiddleware);
+
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
@@ -45,20 +62,20 @@ app.use('/auth', authRoutes);
 
 async function start() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/shop', {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true, 
       useUnifiedTopology: true,
       useFindAndModify: false
     });
-    const candidate = await User.findOne();
-    if (!candidate) {
-      const user = new User({
-        email: 'adm@gmail.com',
-        name: 'admin',
-        cart: {items: []}
-      })
-      await user.save();
-    }
+    // const candidate = await User.findOne();
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: 'adm@gmail.com',
+    //     name: 'admin',
+    //     cart: {items: []}
+    //   })
+    //   await user.save();
+    // }
   } catch (error) {
     console.log(error);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,32 @@
 const { Router } = require('express');
+const User = require('../models/user');
 const router = Router();
+
 
 router.get('/login', async (req, res) => {
   res.render('auth/login', {
     title: 'Authorization',
     isLogin: true
   })
+})
+
+router.post('/login', async (req, res) => {
+  const user = await User.findById('5e8f614f04851f0694a9cb7a');
+  req.session.user = user
+  req.session.isAuthenticated = true;
+  req.session.save(err => {
+    if (err) {
+      throw err
+    }
+  })
+  res.redirect('/');
+})
+
+router.get('/logout', async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/auth/login#login');
+  })
+  
 })
 
 module.exports = router;
