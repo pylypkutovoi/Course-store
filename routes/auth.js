7f4e5055050a4a26dfcd12fa6601eca5e18a3165
pylyp <pylyp.kutov@gmail.com>
2020-04-15 21:13:44 +0200

    add register form validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const { validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
 const sendgrid = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
 const keys = require('../keys');
 const regEmail = require('../emails/registration');
 const resetEmail = require('../emails/reset');
+const { registerValidators } = require('../utils/validators');
 const router = Router();
 
 const transporter = nodemailer.createTransport(sendgrid({
@@ -58,22 +60,25 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => {
   try {
-    const {email, name, password, confirm} = req.body;
-    const candidate = await User.findOne({email});
-    if (candidate) {
-      req.flash('regError', 'Email already exists');
-      res.redirect('/auth/login#register');
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10)
-      const user = new User({
-        email, name, password: hashPassword, cart: {items: []}
-      })
-      await user.save();
-      await transporter.sendMail(regEmail(email));
-      res.redirect('/auth/login#login');
+    const {email, name, password} = req.body;
+    
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.flash('regError', errors.array()[0].msg)
+      return res.status(422).redirect('/auth/login#register')
     }
+    
+    const hashPassword = await bcrypt.hash(password, 10)
+    const user = new User({
+      email, name, password: hashPassword, cart: {items: []}
+    })
+    await user.save();
+    await transporter.sendMail(regEmail(email));
+    res.redirect('/auth/login#login');
+    
     
   } catch (error) {
     console.log(error);
