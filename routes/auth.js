b1a8fb01a2908360038fa431cf0fb67d1115437d
pylyp <pylyp.kutov@gmail.com>
2020-04-16 21:26:17 +0200

    add login validators

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 const keys = require('../keys');
 const regEmail = require('../emails/registration');
 const resetEmail = require('../emails/reset');
-const { registerValidators } = require('../utils/validators');
+const { registerValidators, loginValidators } = require('../utils/validators');
 const router = Router();
 
 const transporter = nodemailer.createTransport(sendgrid({
@@ -31,9 +31,17 @@ router.get('/logout', async (req, res) => {
   })
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidators, async (req, res) => {
   try {
     const {email, password } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.flash('logError', errors.array()[0].msg)
+      return res.status(422).redirect('/auth/login#login')
+    }
+
     const candidate = await User.findOne({ email });
     if (candidate) {
       const isEqual = await bcrypt.compare(password, candidate.password);
@@ -51,10 +59,7 @@ router.post('/login', async (req, res) => {
         req.flash('logError', 'Incorrect password');
         res.redirect('/auth/login#login');
       }
-    } else {
-      req.flash('logError', 'User does not exist');
-      res.redirect('/auth/login#login');
-    }
+    } 
   } catch (error) {
     console.log(error);
   }
--- utils/validators.js
+++ utils/validators.js
@@ -2,23 +2,50 @@ const {body} = require('express-validator');
 const User = require('../models/user');
 
 exports.registerValidators = [
-  body('email', 'Not a valid email address').isEmail().custom(async (value) => {
-    try {
-      const user = await User.findOne({email: value });
-      if (user) {
-        return Promise.reject('Email already exists')
+  body('email', 'Not a valid email address')
+    .isEmail()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({email: value });
+        if (user) {
+          return Promise.reject('Email already exists')
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  }),
-  body('password', 'Password must be at least 6 characters').isLength({min: 6}).isAlphanumeric(),
-  body('confirm').custom((value, {req}) => {
-    if (value !== req.body.password) {
-      throw new Error('Passwords do not match')
-    }
-    return true
-  }),
-  body('name', 'Name must be at least 3 characters').isLength({min: 3})
+    })
+    .normalizeEmail(),
+  body('password', 'Password must be at least 6 characters')
+    .isLength({min: 6})
+    .isAlphanumeric()
+    .trim(),
+  body('confirm')
+    .custom((value, {req}) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match')
+      }
+      return true
+    })
+    .trim(),
+  body('name', 'Name must be at least 3 characters')
+    .isLength({min: 3})
+    .trim()
+]
 
+exports.loginValidators = [
+  body('email', 'Not a valid email address')
+    .isEmail()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({email: value });
+        if (!user) {
+          return Promise.reject('User does not exist')
+        }
+      } catch (error) {
+        console.log(error);
+      }
+  }),
+  body('password', 'Password must be at least 6 characters')
+    .isLength({min: 6})
+    .isAlphanumeric()
 ]
